82c2f32298e7bcff31d44292ec4fccabf2a2df9e
Alif Dewantara <aputradewantara@outlook.com>
2024-12-29 16:01:59 +0700

    implement question card

--- constants/routes.ts
+++ constants/routes.ts
@@ -4,6 +4,7 @@ const ROUTES = {
   SIGN_UP: "/sign-up",
   PROFILE: (id: string) => `/profile/${id}`,
   QUESTION: (id: string) => `/question/${id}`,
+  ASK_QUESTION: "/ask-question",
   TAGS: (id: string) => `/tags/${id}`,
 };
 
