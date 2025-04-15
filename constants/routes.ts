2e25e17ed272ec538294baf3f4b9602d839539ac
Alif Dewantara <aputradewantara@outlook.com>
2025-04-15 14:39:50 +0700

    Implement question form logic

--- constants/routes.ts
+++ constants/routes.ts
@@ -3,7 +3,7 @@ const ROUTES = {
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
   PROFILE: (id: string) => `/profile/${id}`,
-  QUESTION: (id: string) => `/question/${id}`,
+  QUESTION: (id: string) => `/questions/${id}`,
   ASK_QUESTION: "/ask-question",
   TAGS: (id: string) => `/tags/${id}`,
   SIGN_IN_WITH_OAUTH: "signin-with-oauth",
