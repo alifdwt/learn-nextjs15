56b8dece4d1cb795eff5d6284448739236482538
Alif Dewantara <aputradewantara@outlook.com>
2025-04-28 14:10:06 +0700

    Implement edit question

---
+++ app/(root)/questions/[id]/edit/page.tsx
@@ -0,0 +1,29 @@
+import { notFound, redirect } from "next/navigation";
+import React from "react";
+
+import { auth } from "@/auth";
+import QuestionForm from "@/components/forms/QuestionForm";
+import ROUTES from "@/constants/routes";
+import { getQuestion } from "@/lib/actions/question.action";
+import { RouteParams } from "@/types/global";
+
+export default async function EditQuestionPage({ params }: RouteParams) {
+  const { id } = await params;
+  if (!id) return notFound();
+
+  const session = await auth();
+  if (!session) redirect("/sign-in");
+
+  const { data: question, success } = await getQuestion({ questionId: id });
+  if (!success) return notFound();
+
+  //   console.log("question_author_id", question?.author);
+  console.log("question", question);
+  if (question?.author !== session?.user?.id) redirect(ROUTES.QUESTION(id));
+
+  return (
+    <main>
+      <QuestionForm question={question} isEdit />
+    </main>
+  );
+}
