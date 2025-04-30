13721c01d45cdfe1d784f466b5a175573f412502
Alif Dewantara <aputradewantara@outlook.com>
2025-04-30 15:55:47 +0700

    Construct tag page

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,7 +1,52 @@
 import React from "react";
 
-const TagsPage = () => {
-  return <div>TagsPage</div>;
-};
+import TagCard from "@/components/cards/TagCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
+import { EMPTY_TAGS } from "@/constants/state";
+import { getTags } from "@/lib/actions/tag.action";
+import { RouteParams } from "@/types/global";
 
-export default TagsPage;
+export default async function TagPage({ searchParams }: RouteParams) {
+  const { page, pageSize, query, filter } = await searchParams;
+
+  const { success, data, error } = await getTags({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query,
+    filter,
+  });
+
+  const { tags } = data || {};
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900 text-3xl">Tags</h1>
+
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.TAGS}
+          imgSrc="/icons/search.svg"
+          placeholder="Search tags"
+          iconPosition={"left"}
+          otherClasses="flex-1"
+        />
+      </section>
+
+      <DataRenderer
+        success={success}
+        error={error}
+        data={tags}
+        empty={EMPTY_TAGS}
+        render={(tags) => (
+          <div className="mt-10 flex w-full flex-wrap gap-4">
+            {tags.map((tag) => (
+              <TagCard key={tag._id} {...tag} />
+            ))}
+          </div>
+        )}
+      />
+    </>
+  );
+}
---
+++ lib/actions/tag.action.ts
@@ -0,0 +1,139 @@
+import { FilterQuery } from "mongoose";
+
+import { Question } from "@/database";
+import Tag from "@/database/tag.model";
+import { GetTagQuestionParams } from "@/types/action";
+import {
+  ActionResponse,
+  ErrorResponse,
+  PaginatedSearchParams,
+  Question as QuestionType,
+  Tag as TagType,
+} from "@/types/global";
+
+import action from "../handlers/action";
+import handleError from "../handlers/error";
+import {
+  GetTagQuestionSchema,
+  PaginatedSearchParamsSchema,
+} from "../validations";
+
+export async function getTags(
+  params: PaginatedSearchParams
+): Promise<ActionResponse<{ tags: TagType[]; isNext: boolean } | null>> {
+  const validationResult = await action({
+    params,
+    schema: PaginatedSearchParamsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { page = 1, pageSize = 10, query, filter } = params;
+  const skip = (Number(page) - 1) * pageSize;
+  const limit = Number(pageSize);
+
+  const filterQuery: FilterQuery<typeof Tag> = {};
+
+  if (query) {
+    filterQuery.$or = [{ name: { $regex: query, $options: "i" } }];
+  }
+
+  let sortCriteria = {};
+  switch (filter) {
+    case "popular":
+      sortCriteria = { questions: -1 };
+      break;
+    case "recent":
+      sortCriteria = { createdAt: -1 };
+      break;
+    case "oldest":
+      sortCriteria = { createdAt: 1 };
+      break;
+    case "alphabetical":
+      sortCriteria = { name: 1 };
+      break;
+    default:
+      sortCriteria = { questions: -1 };
+      break;
+  }
+
+  try {
+    const totalTags = await Tag.countDocuments(filterQuery);
+
+    const tags = await Tag.find(filterQuery)
+      .sort(sortCriteria)
+      .skip(skip)
+      .limit(limit);
+
+    const isNext = totalTags > skip + tags.length;
+
+    return {
+      success: true,
+      data: { tags: JSON.parse(JSON.stringify(tags)), isNext },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getTagQuestions(params: GetTagQuestionParams): Promise<
+  ActionResponse<{
+    tag: TagType;
+    questions: QuestionType[];
+    isNext: boolean;
+  } | null>
+> {
+  const validationResult = await action({
+    params,
+    schema: GetTagQuestionSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { page = 1, pageSize = 10, query, tagId } = params;
+  const skip = (Number(page) - 1) * pageSize;
+  const limit = Number(pageSize);
+
+  try {
+    const tag = await Tag.findById(tagId);
+    if (!tag) {
+      throw new Error("Tag not found");
+    }
+
+    const filterQuery: FilterQuery<typeof Question> = {
+      tags: { $in: [tagId] },
+    };
+
+    if (query) {
+      filterQuery.title = { $regex: query, $options: "i" };
+    }
+
+    const totalQuestions = await Question.countDocuments(filterQuery);
+
+    const questions = await Question.find(filterQuery)
+      .select("_id title views answers upvotes downvotes author createdAt")
+      .populate([
+        { path: "author", select: "name image" },
+        { path: "tags", select: "name" },
+      ])
+      .skip(skip)
+      .limit(limit);
+
+    const isNext = totalQuestions > skip + questions.length;
+
+    return {
+      success: true,
+      data: {
+        tag: JSON.parse(JSON.stringify(tag)),
+        questions: JSON.parse(JSON.stringify(questions)),
+        isNext,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
